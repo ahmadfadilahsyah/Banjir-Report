f065fa4bea38093bfe328cd6dd450a777e42e020
ahmadfadilahsyah <ahmadfadilahsyah@gmail.com>
2026-06-03 20:14:32 +0700

    Add files via upload

---
+++ banjir_db.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jun 2026 pada 15.13
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banjir_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+-- Kesalahan membaca struktur untuk tabel banjir_db.admin: #1932 - Table &#039;banjir_db.admin&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel banjir_db.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjir_db`.`admin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `laporan`
+--
+-- Kesalahan membaca struktur untuk tabel banjir_db.laporan: #1932 - Table &#039;banjir_db.laporan&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel banjir_db.laporan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjir_db`.`laporan`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
